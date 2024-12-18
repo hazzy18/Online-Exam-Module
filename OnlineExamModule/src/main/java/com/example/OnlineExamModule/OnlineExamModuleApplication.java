6db25992b145326eae387961588bd606c591f44b
LAHAR <lahar0918@gmail.com>
2024-12-18 17:28:34 +0530

    online-exam-module folder structure

---
+++ OnlineExamModule/src/main/java/com/example/OnlineExamModule/OnlineExamModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnlineExamModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamModuleApplication.class, args);
+	}
+
+}
---
+++ OnlineExamModule/src/test/java/com/example/OnlineExamModule/OnlineExamModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnlineExamModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
